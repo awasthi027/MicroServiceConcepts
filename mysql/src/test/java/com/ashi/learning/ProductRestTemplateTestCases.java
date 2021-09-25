1e19a4263c9d7c27560621e6dc2db513f561ee82
inaawasthi20 <myemail.awasthi@gmail.com>
2021-09-25 10:28:30 +0530

    #0009 activate profile like dev, testing, production, qa ect from properties file

--- mysql/src/test/java/com/ashi/learning/ProductRestTemplateTestCases.java
+++ mysql/src/test/java/com/ashi/learning/ProductRestTemplateTestCases.java
@@ -3,16 +3,23 @@ package com.ashi.learning;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
 import com.ashi.learning.entities.Product;
 
+@SpringBootTest
 class ProductRestTemplateTestCases {
 
+	@Value("${mysql.services.url}")
+	private String baseURL;
+	
 	@Test
 	void testGetProduct() {
-		RestTemplate template = new RestTemplate();
-	  Product product = template.getForObject("http://localhost:8080/v1/product/1", Product.class);
+		System.out.println("BaseURL======" + baseURL);
+	  RestTemplate template = new RestTemplate();
+	  Product product = template.getForObject(baseURL + "product/1", Product.class);
 	  assertNotNull(product);
 	  assertEquals("Ceiling Fan", product.getName());
 		
@@ -26,7 +33,7 @@ class ProductRestTemplateTestCases {
 		createProduct.setDescription("Motor bike is great product and having great riding experience.");
 		createProduct.setPrice(700000);
 		
-	   Product newProduct = template.postForObject("http://localhost:8080/v1/createproduct", 
+	   Product newProduct = template.postForObject(baseURL + "createproduct", 
 			   createProduct,Product.class);
 	   assertNotNull(newProduct);
 	   assertNotNull(newProduct.getId());
@@ -36,10 +43,10 @@ class ProductRestTemplateTestCases {
 	@Test
 	 void updateProduct() {
 		RestTemplate template = new RestTemplate();
-		Product product = template.getForObject("http://localhost:8080/v1/product/1", Product.class);
+		Product product = template.getForObject(baseURL + "product/1", Product.class);
 		product.setPrice(1000);
-		template.put("http://localhost:8080/v1/updateproduct/1", product);
-         product = template.getForObject("http://localhost:8080/v1/product/1", Product.class);
+		template.put(baseURL + "updateproduct/1", product);
+         product = template.getForObject(baseURL + "product/1", Product.class);
          assertEquals(1000, product.getPrice());
 		
 	}
